a28db20cddebaea53ed870b029125d0c74616626
frew00 <portantewilfredo@gmail.com>
2025-05-19 11:08:29 +0800

    Initial commit

---
+++ bmwsis/src/main/java/com/sisregistration/bmwsis/Firstcontroller.java
@@ -0,0 +1,27 @@
+package com.sisregistration.bmwsis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Firstcontroller {
+    @GetMapping("/sis")
+    public String welcome() {
+        return "welcome";
+    }
+
+    @GetMapping("/student")
+    public String student() {
+        return "student"; // This will render student.html
+    }
+
+    @GetMapping("/faculty")
+    public String faculty() {
+        return "faculty"; // This will render faculty.html
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
